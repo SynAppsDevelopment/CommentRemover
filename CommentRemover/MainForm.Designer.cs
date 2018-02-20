6e9b422f18fc4abe95dd276839d31a26a2975115
Joe Misko <github@synappsdev.com>
2018-02-19 23:29:14 -0500

    1.0.0.0. Initial project checkin.

---
+++ CommentRemover/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CommentRemover
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.FolderTextBox = new System.Windows.Forms.TextBox();
+			this.BrowseButton = new System.Windows.Forms.Button();
+			this.RemoveCommentsButton = new System.Windows.Forms.Button();
+			this.FBDialog = new System.Windows.Forms.FolderBrowserDialog();
+			this.FilesProgressBar = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(17, 16);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(118, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Solution Folder";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(139, 39);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(300, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "This folder and all subfolders will be searched.";
+			// 
+			// FolderTextBox
+			// 
+			this.FolderTextBox.Location = new System.Drawing.Point(142, 13);
+			this.FolderTextBox.Name = "FolderTextBox";
+			this.FolderTextBox.Size = new System.Drawing.Size(500, 23);
+			this.FolderTextBox.TabIndex = 2;
+			// 
+			// BrowseButton
+			// 
+			this.BrowseButton.Location = new System.Drawing.Point(648, 12);
+			this.BrowseButton.Name = "BrowseButton";
+			this.BrowseButton.Size = new System.Drawing.Size(75, 27);
+			this.BrowseButton.TabIndex = 3;
+			this.BrowseButton.Text = "Browse..";
+			this.BrowseButton.UseVisualStyleBackColor = true;
+			this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+			// 
+			// RemoveCommentsButton
+			// 
+			this.RemoveCommentsButton.Location = new System.Drawing.Point(142, 73);
+			this.RemoveCommentsButton.Name = "RemoveCommentsButton";
+			this.RemoveCommentsButton.Size = new System.Drawing.Size(151, 35);
+			this.RemoveCommentsButton.TabIndex = 4;
+			this.RemoveCommentsButton.Text = "Remove Comments";
+			this.RemoveCommentsButton.UseVisualStyleBackColor = true;
+			this.RemoveCommentsButton.Click += new System.EventHandler(this.RemoveCommentsButton_Click);
+			// 
+			// FBDialog
+			// 
+			this.FBDialog.Description = "Select Solution Folder";
+			this.FBDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+			this.FBDialog.ShowNewFolderButton = false;
+			// 
+			// FilesProgressBar
+			// 
+			this.FilesProgressBar.Location = new System.Drawing.Point(300, 73);
+			this.FilesProgressBar.Minimum = 1;
+			this.FilesProgressBar.Name = "FilesProgressBar";
+			this.FilesProgressBar.Size = new System.Drawing.Size(423, 34);
+			this.FilesProgressBar.Step = 1;
+			this.FilesProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			this.FilesProgressBar.TabIndex = 6;
+			this.FilesProgressBar.Value = 1;
+			this.FilesProgressBar.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(742, 123);
+			this.Controls.Add(this.FilesProgressBar);
+			this.Controls.Add(this.RemoveCommentsButton);
+			this.Controls.Add(this.BrowseButton);
+			this.Controls.Add(this.FolderTextBox);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.Text = "C# Comment Remover";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox FolderTextBox;
+		private System.Windows.Forms.Button BrowseButton;
+		private System.Windows.Forms.Button RemoveCommentsButton;
+		private System.Windows.Forms.FolderBrowserDialog FBDialog;
+		private System.Windows.Forms.ProgressBar FilesProgressBar;
+	}
+}
+
---
+++ CommentRemover/MainForm.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CommentRemover
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			FBDialog.SelectedPath = FolderTextBox.Text;
+		}
+
+		private void RemoveCommentsButton_Click(object sender, EventArgs e)
+		{
+			const string FILE_EXT = "cs";
+
+			Cursor = Cursors.WaitCursor;
+
+			if (String.IsNullOrWhiteSpace(FolderTextBox.Text))
+			{
+				Cursor = Cursors.Default;
+				MessageBox.Show("Please select a solution folder.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+
+			if (!Directory.Exists(FolderTextBox.Text))
+			{
+				Cursor = Cursors.Default;
+				MessageBox.Show("This folder does not exist.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			string[] FileNames = Directory.GetFiles(FolderTextBox.Text, "*." + FILE_EXT, SearchOption.AllDirectories);
+
+			if (0 == FileNames.Length)
+			{
+				Cursor = Cursors.Default;
+				MessageBox.Show("There are no *." + FILE_EXT + " files within this folder.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+
+			Cursor = Cursors.Default;
+
+			if (DialogResult.No == MessageBox.Show("This operation will overwrite all *." + FILE_EXT + " files in your selected folder that have comments in them, " +
+			                                       "except *.Designer." + FILE_EXT + " files and files in folders Properties, bin, obj, .vs, .git, .hg, and .svn." +
+																Environment.NewLine + Environment.NewLine + "Do you want to continue?",
+																this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+				return;
+
+			Cursor = Cursors.WaitCursor;
+
+			FilesProgressBar.Visible = true;
+			FilesProgressBar.Value = FilesProgressBar.Minimum;
+
+			string ExFileName = null;
+
+			try
+			{
+				FilesProgressBar.Maximum = FileNames.Length;
+
+				foreach (string FileName in FileNames)
+				{
+					ExFileName = FileName;
+
+					// Only process files that pass the following exclusions.
+					// 1. Don't modify designer files.
+					if (!FileName.EndsWith(".Designer." + FILE_EXT, StringComparison.CurrentCultureIgnoreCase) &&
+						// 2. Exclude folders.
+						!(FileName.Contains(@"\Properties\") ||
+						  FileName.Contains(@"\obj\") ||
+						  FileName.Contains(@"\bin\") ||
+						  FileName.Contains(@"\.vs\") ||
+						  FileName.Contains(@"\.git\") ||
+						  FileName.Contains(@"\.hg\") ||
+						  FileName.Contains(@"\.svn\")))
+					{
+						RemoveComments(FileName);
+					}
+
+					FilesProgressBar.PerformStep();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error processing file '" + ExFileName + "'." + Environment.NewLine + Environment.NewLine + ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			finally
+			{
+				FilesProgressBar.Visible = false;
+				Cursor = Cursors.Default;
+			}
+
+			MessageBox.Show("Comments removed.", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void RemoveComments(string FileName)
+		{
+			const string TWO_SLASH = "//";
+			const string DOUBLE_QUOTE = "\"";
+
+			bool ChangedFile = false;
+
+			Int64 FileSize = new FileInfo(FileName).Length;
+			System.Text.StringBuilder sb = new System.Text.StringBuilder(Convert.ToInt32(FileSize));
+
+			using (StreamReader sr = new StreamReader(FileName))
+			{
+				string Line;
+				int TwoSlashPos;
+				int LastDoubleQuotePos;
+				string LeftLine;
+
+				while ((Line = sr.ReadLine()) != null)
+				{
+					// Check for two slashes. Don't check for last in case there is (1) a commented comment,
+					// (2) other in-comment usage like http, or (3) a C# documentation triple slash.
+					// Example 1: //int i = 2; // Start loop at 2.
+					// Example 2: // http://www.microsoft.com
+					// Example 3: /// <summary>Retrieve all LabTech Computers.</summary>
+
+					TwoSlashPos = Line.IndexOf(TWO_SLASH, StringComparison.CurrentCulture);
+
+					// If comment not found, write (keep) line. The file will not be changed unless we detect a comment.
+					if (TwoSlashPos == -1)
+						sb.AppendLine(Line);
+
+					else
+					{
+						// Get left side of two slashes.
+						LeftLine = Line.Substring(0, TwoSlashPos);
+
+						// If left side of the two slashes is whitespace, do nothing with this detected whole-line comment.
+						// It will not be added to the StringBuilder and thus removed
+						// from the output file because there appears to be no code.
+						if (String.IsNullOrWhiteSpace(LeftLine))
+						{
+							ChangedFile = true;
+						}
+						else
+						{
+							// If left side of two slashes is not whitespace (probable code),
+							// process and add non-comment portion to StringBuilder.
+							LastDoubleQuotePos = Line.LastIndexOf(DOUBLE_QUOTE);
+							if (LastDoubleQuotePos == -1)
+							{
+								// If no double quote, this is a simple comment. Just add code (non-comment) portion of Line.
+								sb.AppendLine(LeftLine);
+								ChangedFile = true;
+							}
+							else
+							{
+								// If left side contains a double quote (usually a string literal), re-check for comment
+								// at end (after last double quote), because it could be the string that contains two slashes.
+								// Example of comment: string uri = "http://www.microsoft.com/"; // Website.
+								// Example of code only: UriAuthority = Protocol + "://" + ServerName + ":" + PortNumber + "/";
+
+								// TODO: This won't handle " in the comment, such as with the examples below.
+								//       It will write the entire line without stripping the comment.
+								//       To handle this, remove everything after the final two slashes.
+								// Example: s = "\""; // Use " character.
+								// Example: s = "Text"; // Use "Text" string.
+
+								// TODO: This won't handle mmultiple comments with double quotes, such as the examples below.
+								// Example: s = "Text"; // Strings are enclosed within ". // Reset the "handler" string.
+								// Example: s = "Text"; // Reset the "handler" string. // Strings are enclosed within ".
+
+								// TODO: This won't handle /* */ comments.
+
+								// Search for first two slashes after last double quote.
+								TwoSlashPos = Line.IndexOf(TWO_SLASH, LastDoubleQuotePos, StringComparison.CurrentCulture);
+
+								// If no two slashes after last double quote, there is no comment (detected, see TODO),
+								// so add entire line. The file is not changed.
+								if (TwoSlashPos == -1)
+								{
+									sb.AppendLine(Line);
+								}
+								else
+								{
+									// Found two slashes (comment) after last double quote (end of string literal).
+									// Add code (non-comment) portion of Line, the left side before the two slashes.
+									LeftLine = Line.Substring(0, TwoSlashPos);
+									sb.AppendLine(LeftLine);
+									ChangedFile = true;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			if (ChangedFile)
+				File.WriteAllText(FileName, sb.ToString());
+		}
+
+		private void BrowseButton_Click(object sender, EventArgs e)
+		{
+			if (DialogResult.OK == FBDialog.ShowDialog())
+				FolderTextBox.Text = FBDialog.SelectedPath;
+		}
+
+		private void CloseButton_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+	}
+}
